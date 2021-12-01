8e1e11dc9afdcf5073c58da679f7b1237b4f668c
mthomas1331 <mthomas6298@outlook.com>
2021-12-01 12:31:58 +0000

    main file updated to call the challenges

--- src/com/spartaglobal/Main.java
+++ src/com/spartaglobal/Main.java
@@ -1,8 +1,26 @@
 package com.spartaglobal;
 
+import com.spartaglobal.challenge_1.Challenge_1;
+import com.spartaglobal.challenge_2.Challenge_2;
+
+import java.util.ArrayList;
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final int limit = 1000;
+
+        Challenge_1 challenge_1 = new Challenge_1();
+
+        System.out.println(challenge_1.sumOfNumbers(limit));
+
+        Challenge_2 challenge_2 = new Challenge_2();
+        String sentence = "here is a block of text with differing sized words";
+        challenge_2.sortByLength(sentence);
+
+
+
     }
 }
