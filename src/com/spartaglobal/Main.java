0f3664c3371196213be2de325666ab79f288fae0
mthomas1331 <mthomas6298@outlook.com>
2021-12-01 12:42:30 +0000

    Main file updated to show challenges 1 & 2

--- src/com/spartaglobal/Main.java
+++ src/com/spartaglobal/Main.java
@@ -11,13 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
         final int limit = 1000;
+        final String sentence = "here is a block of text with differing sized words";
 
+        //Calls the challenge_1 class to display the result
         Challenge_1 challenge_1 = new Challenge_1();
-
         System.out.println(challenge_1.sumOfNumbers(limit));
 
+        //Calls the challenge_2 class to show result through the method
         Challenge_2 challenge_2 = new Challenge_2();
-        String sentence = "here is a block of text with differing sized words";
         challenge_2.sortByLength(sentence);
 
 
