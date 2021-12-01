46593cc2f60aaeb7a77a2d98e4779808f3ab978f
mthomas1331 <mthomas6298@outlook.com>
2021-12-01 17:25:53 +0000

    Updated the main file to included all the challenges

--- src/com/spartaglobal/Main.java
+++ src/com/spartaglobal/Main.java
@@ -1,27 +1,26 @@
 package com.spartaglobal;
 
-import com.spartaglobal.challenge_1.Challenge_1;
-import com.spartaglobal.challenge_2.Challenge_2;
-
-import java.util.ArrayList;
-
-
+import com.spartaglobal.challenges.challenge_1.Challenge1;
+import com.spartaglobal.challenges.challenge_2.Challenge2;
+import com.spartaglobal.challenges.challenge_3.Challenge3;
 
 public class Main {
 
     public static void main(String[] args) {
         final int limit = 1000;
         final String sentence = "here is a block of text with differing sized words";
+        final String fullName = "Melvin Thomas";
 
-        //Calls the challenge_1 class to display the result
-        Challenge_1 challenge_1 = new Challenge_1();
-        System.out.println(challenge_1.sumOfNumbers(limit));
+        //Calls the challenge1 class to display the result
+        Challenge1 challenge1 = new Challenge1();
+        challenge1.sumOfNumbers(limit);
 
-        //Calls the challenge_2 class to show result through the method
-        Challenge_2 challenge_2 = new Challenge_2();
-        challenge_2.sortByLength(sentence);
+        //Calls the challenge2 class to show result through the method
+        Challenge2 challenge2 = new Challenge2();
+        challenge2.sortByLength(sentence);
 
-
-
+        //Calls the challenge3 class to show the result of reversing the names passed
+        Challenge3 challenge3 = new Challenge3();
+        challenge3.reverseName(fullName);
     }
 }
