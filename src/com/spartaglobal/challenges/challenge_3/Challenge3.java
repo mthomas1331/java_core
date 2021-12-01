b20547d5537d61249f6972f6927a82cf9373a4cb
mthomas1331 <mthomas6298@outlook.com>
2021-12-01 17:23:43 +0000

    Challenge 3 reverse name complete

--- src/com/spartaglobal/challenges/challenge_3/Challenge3.java
+++ src/com/spartaglobal/challenges/challenge_3/Challenge3.java
@@ -1,2 +1,14 @@
-package com.spartaglobal.challenges.challenge_3;public class Challenge3 {
+package com.spartaglobal.challenges.challenge_3;
+
+public class Challenge3 {
+
+    public void reverseName(String fullName){
+        //Find the position of the space in string
+        int spaceId = fullName.indexOf(" ");
+        //Use substring to split the string into two names
+        String firstName = fullName.substring(0, spaceId);
+        String lastName = fullName.substring(spaceId+ 1);
+        //output the final result
+        System.out.println(lastName + " " + firstName);
+    }
 }
